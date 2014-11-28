cb6c72edd9b059384e8649a8c21c11d1ad1c2ac0
northmule <northmule@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-28 06:34:13 +0000

    Релиз плагина "Без оплаты"
    git-svn-id: https://plugins.svn.wordpress.org/without-payment-for-woocommerce/trunk@1034275 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,37 @@
+=== Without payment for WooCommerce ===
+Contributors: northmule
+Donate link: http://www.zixn.ru/
+Tags: without, payment getaway, woo commerce, woocommerce, ecommerce
+Requires at least: 3.6
+Tested up to: 4.1
+Stable tag: 4.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+ 		
+Payment gateway woocommeerce, without charge or obligation. Payment for the goods only after a call to the store Manager.
+
+== Description ==
+
+Платёжный шлюз woocommeerce, без оплаты и обязательств. Оплата товара только после звонка менеджера магазина.
+Платёжный шлюз подойдёт интернет магазином не определившимся с оплатой или если вы предлагаете консультацию по товару или услуги и только потом предлагаете клиенту способы олпаты.
+
+Если возникнут проблемы, [сообщите об ошибке](http://www.zixn.ru/plagin-payment-woocommerce.html)
+
+
+== Installation ==
+1. Убедитесь что у вас установлена посленяя версия плагина [WooCommerce](/www.woothemes.com/woocommerce)
+2. Распакуйте архив и загрузите "without-payment-woocommerce" в папку ваш-домен/wp-content/plugins
+3. Активируйте плагин
+
+== Screenshots ==
+1. Общие настройки шлюза. 
+2. То что видят клиенты.
+3. Подтверждение заказа.
+4. Ваша страница уведомления о подтверждение заказа.
+
+
+== Changelog ==
+= 1.0 =
+* Релиз
+
---
+++ with-geteway-index.php
@@ -0,0 +1,277 @@
+<?php
+/*
+  Plugin Name: Without payment woocommerce
+  Plugin URI: http://www.zixn.ru/interkassa-gateway-woocommerce.html
+  Description: Платёжный шлюз woocommeerce, без оплаты и обязательств. Оплата товара только после звонка менеджера магазина.
+  Version: 1.0
+  Author: Djon
+  Author URI: http://zixn.ru
+ */
+
+/*  Copyright 2014  Djon  (email: Ermak_not@mail.ru)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ */
+
+
+
+add_action('plugins_loaded', 'init_without_shluz'); //Подключаем класс с шлюзом
+
+/**
+ * Подключиение класса с платёжной системой
+ */
+function init_without_shluz() {
+
+
+    class WC_WITHOUTGATEWAY extends WC_Payment_Gateway {
+
+        public function __construct() {
+
+            $plugin_dir = plugin_dir_url(__FILE__);
+
+            global $woocommerce;
+
+            $this->id = 'WithOut';
+            $this->icon = apply_filters('woocommerce_without_icon', '' . $plugin_dir . 'without.png');
+            $this->has_fields = false;
+// Загрузка настроек
+            $this->init_form_fields();
+            $this->init_settings(); //Получение настроек из woocommerce
+// Define user set variables
+            $this->title = $this->settings['title'];
+            $this->description = $this->settings['description'];
+            $this->instructions = $this->settings['instructions'];
+//$this->redirect_page_id = $this->settings['redirect_page_id'];
+// Logs
+            if ($this->debug == 'yes') {
+                $this->log = $woocommerce->logger();
+            }
+
+// Actions
+//Подтверждалка заказа
+            add_action('woocommerce_receipt_' . $this->id, array($this, 'receipt_page')); // Подтверждение заказа
+// Сохранение настроек
+            add_action('woocommerce_update_options_payment_gateways_' . $this->id, array($this, 'process_admin_options'));
+// Payment listener/API hook
+           // add_action('woocommerce_api_wc_' . $this->id, array($this, 'check_ipn_response'));
+            if (!$this->is_valid_for_use()) {
+                $this->enabled = false;
+            }
+        }
+
+        /**
+         * Проверка прав пользователя
+         */
+        function is_valid_for_use() {
+            if (!in_array(get_option('woocommerce_currency'), array('RUB'))) {
+                return false;
+            }
+            return true;
+        }
+
+        /**
+         * Внешний вид страницы Платёжного шлюза
+         * */
+        public function admin_options() {
+            ?>
+            <h3><?php _e('WithOut', 'woocommerce'); ?></h3>
+            <p><?php _e('Настройка приема электронных платежей через "Без оплаты".', 'woocommerce'); ?></p>
+
+            <?php if ($this->is_valid_for_use()) : ?>
+
+                <table class="form-table">
+
+                    <?php
+                    // Генерация HTML настроек на основе заданных параметров
+                    $this->generate_settings_html();
+                    ?>
+                </table><!--/.form-table-->
+
+            <?php else : ?>
+                <div class="inline error"><p><strong><?php _e('Шлюз отключен', 'woocommerce'); ?></strong>: <?php _e('WithOut не поддерживает валюты Вашего магазина.', 'woocommerce'); ?></p></div>
+            <?php
+            endif;
+        }
+
+// End admin_options()
+
+        /**
+         * Поля для HTML опций шлюза
+         */
+        function init_form_fields() {
+            $this->form_fields = array(
+                'enabled' => array(
+                    'title' => __('Включить/Выключить', 'woocommerce'),
+                    'type' => 'checkbox',
+                    'label' => __('Включен', 'woocommerce'),
+                    'default' => 'yes'
+                ),
+                'title' => array(
+                    'title' => __('Название', 'woocommerce'),
+                    'type' => 'text',
+                    'description' => __('Это название, которое пользователь видит во время проверки.', 'woocommerce'),
+                    'default' => __('Without', 'woocommerce')
+                ),
+                'without_succes' => array(
+                    'title' => __('Страница перенаправления', 'woocommerce'),
+                    'type' => 'select',
+                    'options' => $this->get_pages('Выберите страницу...'),
+                    'description' => "На эту страницу покупатель попадёт после подтверждения заказа, вашу страницу можно оформить по вашему вкусу"
+                ),
+                'debug' => array(
+                    'title' => __('Режим логирования', 'woocommerce'),
+                    'type' => 'checkbox',
+                    'label' => __('Включить логирование (<code>woocommerce/logs/without.txt</code>)', 'woocommerce'),
+                    'default' => 'no'
+                ),
+                'description' => array(
+                    'title' => __('Description', 'woocommerce'),
+                    'type' => 'textarea',
+                    'description' => __('Описанием метода оплаты которое клиент будет видеть на вашем сайте.', 'woocommerce'),
+                    'default' => 'Без оплаты.'
+                )
+            );
+        }
+
+        /**
+         * There are no payment fields for sprypay, but we want to show the description if set.
+         * */
+//        function payment_fields() {
+//            if ($this->description) {
+//                echo wpautop(wptexturize($this->description));
+//            }
+//        }
+
+        /**
+         * Генерация кнопок на странице подтверждения заказа
+         * */
+        public function generate_form($order_id) {
+            global $woocommerce;
+
+            $order = new WC_Order($order_id);
+
+            if ($this->testmode == 'yes') {
+                $action_adr = $this->testurl;
+            } else {
+//                $action_adr = get_permalink($this->settings['without_succes']);  //Получаем опцию из базы woo
+                $action_adr = $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+            }
+            $payu_args = array(
+                'amount' => $order->order_total,
+                'productinfo' => $productinfo,
+                'firstname' => $order->billing_first_name,
+                'lastname' => $order->billing_last_name,
+                'address1' => $order->billing_address_1,
+                'address2' => $order->billing_address_2,
+                'city' => $order->billing_city,
+                'state' => $order->billing_state,
+                'country' => $order->billing_country,
+                'zipcode' => $order->billing_zip,
+                'email' => $order->billing_email,
+                'phone' => $order->billing_phone,
+                'surl' => $action_adr,
+                'furl' => $action_adr,
+                'curl' => $action_adr
+            );
+            $payu_args_array = array();
+            foreach ($payu_args as $key => $value) {
+                $payu_args_array[] = '<input type="hidden" name="' . esc_attr($key) . '" value="' . esc_attr($value) . '" />';
+            }
+
+            return
+                    '<form action="' . esc_url($action_adr) . '" method="POST">' . "\n" .
+                    implode("\n", $payu_args_array) .
+                    '<input type="submit" class="button alt" name="without_pay" value="' . __('Подтвердить', 'woocommerce') . '" /> <a class="button cancel" href="' . $order->get_cancel_order_url() . '">' . __('Отказаться & вернуться в корзину', 'woocommerce') . '</a>' . "\n" .
+                    '</form>';
+        }
+
+        /**
+         * Запускает механизм после выбора оплаты на странице ввода данных о клиенте
+         * */
+        function process_payment($order_id) {
+            $order = new WC_Order($order_id);
+
+            return array(
+                'result' => 'success',
+                'redirect' => add_query_arg('order', $order->id, add_query_arg('key', $order->order_key, get_permalink(woocommerce_get_page_id('pay'))))
+            );
+        }
+
+        /**
+         * Страница подтверждения заказ
+         * */
+        function receipt_page($order) {
+            echo '<p>' . __('Спасибо за заказ, для подтверждения заказа - нажмите на кнопку ниже!', 'woocommerce') . '</p>';
+
+            echo $this->generate_form($order); //Кнопки и прочее
+            if (isset($_POST['without_pay'])) {
+                wc_empty_cart();
+                $action_adr = get_permalink($this->settings['without_succes']);
+                wp_redirect($action_adr);
+            }
+        }
+
+        /**
+         * Check Response
+         * */
+//        function check_ipn_response() {
+//            global $woocommerce;
+//            if (isset($_POST['without_pay'])) {
+//                wc_empty_cart();
+//                $action_adr = "http://mail.ru";
+//                wp_redirect($action_adr);
+//            }
+//
+//        }
+
+// Получает страницы сайта
+        function get_pages($title = false, $indent = true) {
+            $wp_pages = get_pages('sort_column=menu_order');
+            $page_list = array();
+            if ($title)
+                $page_list[] = $title;
+            foreach ($wp_pages as $page) {
+                $prefix = '';
+// show indented child pages?
+                if ($indent) {
+                    $has_parent = $page->post_parent;
+                    while ($has_parent) {
+                        $prefix .= ' - ';
+                        $next_page = get_page($has_parent);
+                        $has_parent = $next_page->post_parent;
+                    }
+                }
+// add to page list array array
+                $page_list[$page->ID] = $prefix . $page->post_title;
+            }
+            return $page_list;
+        }
+
+    }
+
+    /**
+     * Функция подключения класса с платёжным шлюзом
+     * */
+    function add_without_gateway($methods) {
+        $methods[] = 'WC_WITHOUTGATEWAY';
+        return $methods;
+    }
+
+    add_filter('woocommerce_payment_gateways', 'add_without_gateway');
+}
+
+// -- конец Подключиение класса с платёжной системой
+?>
